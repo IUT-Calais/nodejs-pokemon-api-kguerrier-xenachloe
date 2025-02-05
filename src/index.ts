d5c94d7b4167a61d297967f1b8cf981e81af343d
Chloé Faillie <chloe.faillie@gmail.com>
2025-02-05 13:42:34 +0100

    pokemon-cards/id

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 
+
 const prisma = new PrismaClient();
 
 export const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 
 export const server = app.listen(port);
 
@@ -15,12 +14,19 @@ export function stopServer() {
   server.close();
 }
 
-
 app.get('/pokemon-cards', async (_req: Request, res: Response) => {
   const pokemon = await prisma.pokemonCard.findMany()
   res.status(200).send(pokemon);
 });
 
-app.get('/pokemons-cards/:id', (_req: Request, res: Response) => {
-  res.status(200).send(`Pokemon ${_req.params.id}`);
+app.get('/pokemon-cards/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const pokemon = await prisma.pokemonCard.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (pokemon) {
+    res.status(200).json(pokemon);
+  } else {
+    res.status(404).send(`Pokémon avec l'ID ${id} non trouvé`);
+  }
 });
